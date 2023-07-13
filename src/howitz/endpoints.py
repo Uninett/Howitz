03671cddba384cfa61fa8c0b6af990d2f01d8a09
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2023-07-13 07:51:52 +0200

    Save before cleanup

--- src/howitz/endpoints.py
+++ src/howitz/endpoints.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 from flask_assets import Bundle, Environment
 
 from itertools import product
 import logging
 from pathlib import Path
-import time
 import datetime
 import curitz
 
-from zinolib.ritz import ritz, notifier, parse_tcl_config
+from zinolib.ritz import ritz, notifier, parse_tcl_config, caseState
 from curitz import cli
 
 app = Flask(__name__)
@@ -19,6 +18,7 @@ css = Bundle("main.css", output="dist/main.css")
 
 assets.register("css", css)
 css.build()
+
 
 class Obj:
     pass
@@ -85,7 +85,6 @@ class Engine:
             self.cases[case.id] = case
 
 
-
 def get_current_cases():
     config = Engine.get_config()
     engine = Engine(config)
@@ -94,6 +93,28 @@ def get_current_cases():
     print('Connected')
     engine.load_current_cases()
     cases = engine.cases
+    print("CASES ITEMS", cases)
+
+    cases_sorted = {k: cases[k] for k in sorted(cases,
+                                            key=lambda k: (
+                                                # 0 if cases[k].get("state") == caseState.IGNORED else 1,
+                                                cases[k].history[-1]['date'],
+                                            ), reverse=True)}
+
+    # cases_sorted = sorted(cases,
+    #                       key=lambda k: (
+    #                           0 if cases[k].get("state") == caseState.IGNORED else 1,
+    #                           cases[k].history[-1]['date'],
+    #                       ))
+
+
+    # cases_sorted = dict(sorted(cases,
+    #                     key=lambda k: (
+    #                         0 if cases[k].get("state") == caseState.IGNORED else 1,
+    #                         cases[k]._attrs["updated"],
+    #                     ),))
+
+    print("CASES SORTED", cases_sorted)
 
     # for key, value in cases.items():
     #     print(f'Event {key}: {value.keys()}')
@@ -101,12 +122,13 @@ def get_current_cases():
     # print('EVENTS ', len(cases))
 
     table_cases = []
-    for c in cases.values():
+    for c in cases_sorted.values():
+        # print('CASE RAW', c.history[-1])
         table_cases.append(create_case(c))
 
     # print('Table cases', table_cases)
 
-    return table_cases, cases, engine
+    return table_cases, cases_sorted, engine
 
 
 def create_case(case):
@@ -159,6 +181,7 @@ def create_case(case):
 
     return common
 
+
 def get_event_attributes(id):
     config = Engine.get_config()
     engine = Engine(config)
@@ -208,7 +231,6 @@ def index():
     return render_template('index.html', example_list=exemplify_loop)
 
 
-
 @app.route('/events')
 def events_table():
     case_list = []
@@ -233,7 +255,6 @@ def events_list():
 
 @app.route('/show_details/<i>', methods=["GET"])
 def read_details(i):
-
     case_attr, event_logs, event_history, event_msgs = get_event_details(i)
 
     return render_template('event-details.html', id=i, case_attr=case_attr, event_logs=event_logs,
@@ -242,19 +263,56 @@ def read_details(i):
 
 @app.route('/hide_details/<i>', methods=["GET"])
 def hide_details(i):
-
     return render_template('hide-event-details.html', id=i)
 
 
-@app.route('/event/<i>/update_status', methods=["GET"])
+@app.route('/event/<i>/update_status', methods=['GET', 'POST'])
 def update_event_status(i):
+    if request.method == 'POST':
+        case_id = int(i)
+        case_attr_current = get_event_details(i)[0]
+        event_current_state = case_attr_current['state'].value
+        print('STATE ENUM', event_current_state)
 
-    return render_template('ui-update-event-status-form.html', id=i)
+        event_state_val = request.form['event-state']
+        event_history_val = request.form['event-history']
+        print('EVENT_STATE', event_state_val)
+        print('EVENT_HISTORY', event_history_val)
+
+        config = Engine.get_config()
+        engine = Engine(config)
+        engine.connect()
+
+        if not event_current_state == event_state_val:
+            set_state_res = engine.session.set_state(case_id, event_state_val)
+            print("SET_STATE RES", set_state_res)
+
+        if event_history_val:
+            add_history_res = engine.session.add_history(case_id, event_history_val)
+            print("ADD_HISTORY RES", add_history_res)
+
+        case_attr, event_logs, event_history, event_msgs = get_event_details(i)
+
+        return render_template('event-details.html', id=i, case_attr=case_attr, event_logs=event_logs,
+                               event_history=event_history, event_msgs=event_msgs)
+
+    elif request.method == 'GET':
+        return render_template('ui-update-event-status-form.html', id=i)
+
+    # # res = make_response(render_template('ui-update-event-status-form.html', id=i))
+    # # res.headers['HX-Reswap'] = f'none show:#ol-event-{i}:bottom'
+    # #
+    # # return res
+    #
+    # return render_template('ui-update-event-status-form.html', id=i)
 
 
 @app.route('/event/<i>/update_status/cancel', methods=["GET"])
 def cancel_update_event_status(i):
+    return render_template('ui-hidden-li.html', id=i)
 
-    return render_template('ui-generic-hidden.html', id=i)
 
-
+# TODO: replace this with some other HTMX pattern
+@app.route('/get_none', methods=["GET"])
+def get_none():
+    return render_template('ui-generic-hidden.html')
