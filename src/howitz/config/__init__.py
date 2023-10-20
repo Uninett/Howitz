1f30afca1ef88918a8ed078d841028adbad319f7
Hanne Moa <hanne.moa@sikt.no>
2023-10-20 12:48:55 +0200

    Add support for reading config from file

--- src/howitz/config/__init__.py
+++ src/howitz/config/__init__.py
@@ -1,20 +1,43 @@
 from pathlib import Path
 
+from zinolib.config.toml import parse_toml_config
 from .models import HowitzConfig, DevHowitzConfig
 
 
 class Config:
 
-    @staticmethod
-    def from_args(args):
+    @classmethod
+    def get_class(cls, devmode=False):
+        classobj = DevHowitzConfig if devmode else HowitzConfig
+        return type("Config", (classobj, cls), {})
+
+    @classmethod
+    def from_args(cls, args):
         "Assumes argparse-style args namespace object"
         config_dict = {
             'port': args.port,
             'listen': args.listen,
             'storage': args.storage,
         }
-        cls = DevHowitzConfig if args.devmode else HowitzConfig
-        return cls(**config_dict)
+        Config = cls.get_class(args.devmode)
+        return Config(**config_dict)
+
+    @classmethod
+    def from_toml(cls,filename=None):
+        """Format:
+
+        [howitz]
+        listen = "127.0.0.1"
+        port = 9000
+        storage = "./howitz.sqlite3"
+        devmode = false
+        """
+        if not filename:
+            filename = 'howitz.toml'
+        config_dict = parse_toml_config(filename)
+        howitz_dict = config_dict['howitz']
+        Config = cls.get_class(howitz_dict.get("devmode"))
+        return Config(**howitz_dict)
 
     def update_from_args(self, args):
         """
@@ -24,5 +47,5 @@ class Config:
         """
         for arg in vars(args):
             value = getattr(args, arg, None)
-            if arg in vars(self):
+            if value and arg in vars(self):
                 setattr(self, arg, value)
