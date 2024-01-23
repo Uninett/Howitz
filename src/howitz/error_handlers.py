33b0058f351f884dc5b0a141f16e1904f907cc0d
Hanne Moa <hanne.moa@sikt.no>
2024-01-23 08:39:04 +0100

    Fix missing error-key in session

--- src/howitz/error_handlers.py
+++ src/howitz/error_handlers.py
@@ -12,6 +12,8 @@ def handle_generic_http_exception(e):
     alert_random_id = str(uuid.uuid4())
     short_err_msg = f"{e.code} {e.name}: {e.description}"
 
+    if not "errors" in session:
+        session["errors"] = dict()
     session["errors"][str(alert_random_id)] = serialize_exception(e)
     session.modified = True
     current_app.logger.debug('ERRORS %s', session["errors"])
