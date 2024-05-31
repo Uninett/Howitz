3fab5510c8e13a25b03835743eb0d95cbab79e2d
Hanne Moa <hanne.moa@sikt.no>
2024-05-31 13:56:19 +0200

    Update to use zinolib 1.1

--- src/howitz/__init__.py
+++ src/howitz/__init__.py
@@ -54,6 +54,10 @@ def create_app(test_config=None):
     app.event_manager = event_manager
     app.logger.debug('Zino1EventManager %s', event_manager)
 
+    # set up placeholder for UpdateHandler
+    app.updater = None
+    app.logger.debug('UpdateHandler is None')
+
     # set up user database
     database = UserDB(app.config["HOWITZ_STORAGE"])
     database.initdb()
