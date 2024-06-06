3f4a47ec6f82a1e9f57f6dcc5d9c05eb35fcb411
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-06-06 11:15:19 +0200

    Register lost connection handler in the app

--- src/howitz/__init__.py
+++ src/howitz/__init__.py
@@ -12,10 +12,10 @@ from werkzeug.exceptions import HTTPException, BadRequest, NotFound, Forbidden
 from howitz.config.utils import load_config
 from howitz.config.zino1 import make_zino1_config
 from howitz.config.howitz import make_howitz_config
-from howitz.error_handlers import handle_generic_exception, handle_generic_http_exception, handle_400, handle_404, handle_403
+from howitz.error_handlers import handle_generic_exception, handle_generic_http_exception, handle_400, handle_404, handle_403, handle_lost_connection
 from howitz.users.db import UserDB
 from howitz.users.commands import user_cli
-from zinolib.controllers.zino1 import Zino1EventManager
+from zinolib.controllers.zino1 import Zino1EventManager, LostConnectionError, NotConnectedError
 
 
 __all__ = ["create_app"]
@@ -29,7 +29,10 @@ def create_app(test_config=None):
     app.register_error_handler(HTTPException, handle_generic_http_exception)
     app.register_error_handler(BadRequest, handle_400)
     app.register_error_handler(NotFound, handle_404)
-    app.register_error_handler(Forbidden, handle_403),
+    app.register_error_handler(Forbidden, handle_403)
+    app.register_error_handler(LostConnectionError, handle_lost_connection)
+    app.register_error_handler(BrokenPipeError, handle_lost_connection)
+    app.register_error_handler(NotConnectedError, handle_lost_connection)
 
     # load config
     app = load_config(app, test_config)
