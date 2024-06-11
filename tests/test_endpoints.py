7a5722afdf7c08efe87e9f9741ec153b64e2cc31
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-06-11 14:55:50 +0200

    Add more tests for sort_events helper

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,7 +1,7 @@
 import random
 import pytest
 from datetime import datetime, timezone
-from zinolib.event_types import Event, AdmState, PortState, BFDState, ReachabilityState
+from zinolib.event_types import Event, AdmState, PortState, BFDState, ReachabilityState, PortStateEvent
 from howitz.endpoints import sort_events, EventSort, get_priority
 from flask import Flask
 
@@ -20,7 +20,94 @@ class TestSortEvents:
         prev_event = None
         for event_id, event in sorted_events.items():
             if prev_event:
-                assert prev_event.updated <= event.updated, "Older event seen after younger event"
+                assert prev_event.updated <= event.updated, "Older updated event seen after more recently updated event"
+            prev_event = event
+
+    def test_upd_rev_sorting_should_order_by_youngest_updated_value_first(self, events):
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.UPD_REV)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert prev_event.updated >= event.updated, "More recently updated event seen after older updated event"
+            prev_event = event
+
+    def test_age_sorting_should_order_by_youngest_created_value_first(self, events):
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.AGE)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert prev_event.opened >= event.opened, "More recently created event seen after older created event"
+            prev_event = event
+
+    def test_age_rev_sorting_should_order_by_oldest_created_value_first(self, events):
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.AGE_REV)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert prev_event.opened <= event.opened, "Older created event seen after more recently created event"
+            prev_event = event
+
+    def test_lasttrans_sorting_should_order_by_youngest_updated_value_first(self, events):
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.LASTTRANS)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert prev_event.updated >= event.updated, "Event with more recent last transaction seen after event with older last transaction"
+            prev_event = event
+
+    def test_lasttrans_sorting_should_put_ignored_events_last_regardless_of_their_updated_value(self, lasttrans_events):
+        events = lasttrans_events
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.LASTTRANS)
+        is_detected_ignored = None
+        for event_id, event in sorted_events.items():
+            if is_detected_ignored:
+                assert event.adm_state == AdmState.IGNORED, "Event with status other than IGNORED seen after event with status IGNORED"
+            else:
+                is_detected_ignored = event.adm_state == AdmState.IGNORED
+
+    def test_down_sorting_should_order_by_longest_downtime_value_first(self, downtime_events):
+        events = downtime_events
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.DOWN)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert prev_event.get_downtime() >= event.get_downtime(), "Event with longer downtime seen after event with shorter downtime"
+            prev_event = event
+
+    def test_down_rev_sorting_should_order_by_shortest_downtime_value_first(self, downtime_events):
+        events = downtime_events
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.DOWN_REV)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert prev_event.get_downtime() <= event.get_downtime(), "Event with shorter downtime seen after event with longer downtime"
+            prev_event = event
+
+    def test_severity_sorting_should_order_by_highest_priority_first(self, get_priority_boilerplate_event):
+        events = {}
+
+        event_with_low_prio = Event.create({**get_priority_boilerplate_event, 'type': Event.Type.PORTSTATE, 'adm_state': AdmState.IGNORED, 'port_state': PortState.UP})
+        events[event_with_low_prio.id] = event_with_low_prio
+
+        event_with_medium_prio = Event.create({**get_priority_boilerplate_event, 'type': Event.Type.PORTSTATE, 'adm_state': AdmState.OPEN, 'port_state': PortState.UP})
+        events[event_with_medium_prio.id] = event_with_medium_prio
+
+        event_with_high_prio = Event.create({**get_priority_boilerplate_event, 'type': Event.Type.PORTSTATE, 'adm_state': AdmState.OPEN, 'port_state': PortState.DOWN})
+        events[event_with_high_prio.id] = event_with_high_prio
+
+        with test_app.app_context():
+            sorted_events = sort_events(events, sort_by=EventSort.SEVERITY)
+        prev_event = None
+        for event_id, event in sorted_events.items():
+            if prev_event:
+                assert get_priority(prev_event) <= get_priority(event), "Event with higher priority seen after event with lower priority"
             prev_event = event
 
 
@@ -37,6 +124,42 @@ def events():
             router="router1",
             opened=now,
             updated=now,
+        )
+        events_dict[event_id] = event
+    return events_dict
+
+
+@pytest.fixture()
+def lasttrans_events():
+    events_dict = {}
+    for i in range(5):
+        event_id = i+1
+        event = Event(
+            id=event_id,
+            type=Event.Type.PORTSTATE,
+            adm_state=AdmState.OPEN if event_id % 2 == 0 else AdmState.IGNORED,
+            router="router1",
+            opened=i*200,
+            updated=i*100,
+        )
+        events_dict[event_id] = event
+    return events_dict
+
+
+@pytest.fixture()
+def downtime_events():
+    events_dict = {}
+    for i in range(5):
+        event_id = i+1
+        event = PortStateEvent(
+            id=event_id,
+            adm_state=AdmState.OPEN,
+            router="router1",
+            opened=i*200,
+            updated=i*100,
+            ac_down=i*50,
+            if_index=1234,
+            port_state=PortState.UP
         )
         events_dict[event_id] = event
     return events_dict
