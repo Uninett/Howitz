5a2999f087b0bbffb675720d407545186bbc8b20
podliashanyk <60876078+podliashanyk@users.noreply.github.com>
2024-06-19 12:34:06 +0200

    Fix stale columns data (#127)
    * Shorten downtime info in the column
    
    Uses a helper where implementation is copied from curitz.
    Formatting downtime info in the column this way removes the stale-data-problem for this column completely. This is because there are no more redundant time units (that need constant updates) that are shown in the column, so whenever downtime for event is changed this info is updated in the specific row via NTIE.
    
    * Add util helper for calculating formatted event age
    
    * Use util for calculating event age
    
    * Store timestamp of last active events data refresh in session
    
    This timestamp is updated only if all events attributes and computed fields were actively refreshed using create_table_event()
    
    * Refresh event data fields if more than minute since last active refresh
    
    No zinolib is called, just recalculation of event internal data

--- src/howitz/utils.py
+++ src/howitz/utils.py
@@ -1,6 +1,6 @@
 import functools
 import traceback
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from flask import current_app
 from flask_login import current_user
@@ -52,3 +52,31 @@ def set_correct_timezone(dt: datetime):
 def date_str_without_timezone(dt: datetime):
     dt_aware = set_correct_timezone(dt)
     return dt_aware.strftime("%Y-%m-%d %H:%M:%S")
+
+
+# Implementation copied from curitz
+def shorten_downtime(td: timedelta):
+    """
+    Shortens downtime to single greatest applicable timedelta unit (days, hours, minutes, seconds).
+    :param td: downtime as `datetime.timedelta`
+    :return: formatted downtime as string
+    """
+    if td.days > 0:
+        return "{:2d}d".format(td.days)
+    if td.seconds < 60:
+        return "{:2d}s".format(td.seconds)
+    if td.seconds / 60 < 60:
+        return "{:2.0f}m".format(td.seconds / 60)
+    if td.seconds / 60 / 60 < 60:
+        return "{:2.0f}h".format(td.seconds / 60 / 60)
+
+
+def calculate_event_age_no_seconds(opened: datetime):
+    """
+    Calculate event age and remove seconds.
+    :param opened: event opened time as `datetime.datetime`
+    :return: event age without seconds as string
+    """
+    age = datetime.now(timezone.utc) - opened
+    age_formatted = str(age)[:-10]
+    return age_formatted
